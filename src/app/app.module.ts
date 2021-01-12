e6979741d272359783907a970db31ef6c3082e65
Gajini Ayyappa <Gajini-Ayyappan>
2021-01-12 15:02:39 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminModule } from './admin/admin.module';
+import { CheckComponent } from './check/check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
-    Page2Component
+    Page2Component,
+    CheckComponent
   ],
   imports: [
     BrowserModule,
